dab693ae7026e575755be3b6588b86a7666d3c59
Putu Herdy <45077089+vkurpmax@users.noreply.github.com>
2021-10-30 15:30:36 +0800

    Create Running_LED_01.ino

---
+++ Self Learning/Running LED/Running_LED_01.ino
@@ -0,0 +1,324 @@
+// SKETCH 8 LED VARIASI BY MIRIP TIVI
+// SEMOGA BERMANFAAT
+// JANGAN SUNGKAN UNTUK LIKE, KOMEN, SUBSCRIBE DAN SHARE
+// youtube.com/miriptivi
+// KENDAL 03 JANUARI 2018
+
+int LED1=2;//mengenali Varibel LED1 untuk pin 2
+int LED2=3;//mengenali Varibel LED2 untuk pin 3
+int LED3=4;//mengenali Varibel LED3 untuk pin 4
+int LED4=5;//mengenali Varibel LED4 untuk pin 5
+int LED5=6;//mengenali Varibel LED5 untuk pin 6
+int LED6=7;//mengenali Varibel LED6 untuk pin 7
+int LED7=8;//mengenali Varibel LED7 untuk pin 8
+int LED8=9;//mengenali Varibel LED8 untuk pin 9
+void setup() {
+pinMode(LED1, OUTPUT);// membuat LED1 menjadi output
+pinMode(LED2, OUTPUT);// membuat LED2 menjadi output
+pinMode(LED3, OUTPUT);// membuat LED3 menjadi output
+pinMode(LED4, OUTPUT);// membuat LED4 menjadi output
+pinMode(LED5, OUTPUT);// membuat LED5 menjadi output
+pinMode(LED6, OUTPUT);// membuat LED6 menjadi output
+pinMode(LED7, OUTPUT);// membuat LED7 menjadi output
+pinMode(LED8, OUTPUT);// membuat LED8 menjadi output
+}
+
+void loop() {
+digitalWrite(LED1, HIGH);
+delay(100);
+digitalWrite(LED1, LOW);
+delay(100);
+digitalWrite(LED2, HIGH);
+delay(100);
+digitalWrite(LED2, LOW);
+delay(100);
+digitalWrite(LED3, HIGH);
+delay(100);
+digitalWrite(LED3, LOW);
+delay(100);
+digitalWrite(LED4, HIGH);
+delay(100);
+digitalWrite(LED4, LOW);
+delay(100);
+digitalWrite(LED5, HIGH);
+delay(100);
+digitalWrite(LED5, LOW);
+delay(100);
+digitalWrite(LED6, HIGH);
+delay(100);
+digitalWrite(LED6, LOW);
+delay(100);
+digitalWrite(LED7, HIGH);
+delay(100);
+digitalWrite(LED7, LOW);
+delay(100);
+digitalWrite(LED8, HIGH);
+delay(100);
+digitalWrite(LED8, LOW);
+delay(100);
+
+digitalWrite(LED1, HIGH);
+delay(50);
+digitalWrite(LED2, HIGH);
+delay(50);
+digitalWrite(LED3, HIGH);
+delay(50);
+digitalWrite(LED4, HIGH);
+delay(50);
+digitalWrite(LED5, HIGH);
+delay(50);
+digitalWrite(LED6, HIGH);
+delay(50);
+digitalWrite(LED7, HIGH);
+delay(50);
+digitalWrite(LED8, HIGH);
+delay(50);
+
+digitalWrite(LED8, LOW);
+delay(50);
+digitalWrite(LED7, LOW);
+delay(50);
+digitalWrite(LED6, LOW);
+delay(50);
+digitalWrite(LED5, LOW);
+delay(50);
+digitalWrite(LED4, LOW);
+delay(50);
+digitalWrite(LED3, LOW);
+delay(50);
+digitalWrite(LED2, LOW);
+delay(50);
+digitalWrite(LED1, LOW);
+delay(50);
+
+digitalWrite(LED8, HIGH);
+delay(100);
+digitalWrite(LED8, LOW);
+delay(100);
+digitalWrite(LED7, HIGH);
+delay(100);
+digitalWrite(LED7, LOW);
+delay(100);
+digitalWrite(LED6, HIGH);
+delay(100);
+digitalWrite(LED6, LOW);
+delay(100);
+digitalWrite(LED5, HIGH);
+delay(100);
+digitalWrite(LED5, LOW);
+delay(100);
+digitalWrite(LED4, HIGH);
+delay(100);
+digitalWrite(LED4, LOW);
+delay(100);
+digitalWrite(LED3, HIGH);
+delay(100);
+digitalWrite(LED3, LOW);
+delay(100);
+digitalWrite(LED2, HIGH);
+delay(100);
+digitalWrite(LED2, LOW);
+delay(100);
+digitalWrite(LED1, HIGH);
+delay(100);
+digitalWrite(LED1, LOW);
+delay(100);
+
+digitalWrite(LED1, HIGH);
+digitalWrite(LED2, HIGH);
+digitalWrite(LED3, HIGH);
+digitalWrite(LED4, HIGH);
+digitalWrite(LED5, HIGH);
+digitalWrite(LED6, HIGH);
+digitalWrite(LED7, HIGH);
+digitalWrite(LED8, HIGH);
+delay(200);
+digitalWrite(LED1, LOW);
+digitalWrite(LED2, LOW);
+digitalWrite(LED3, LOW);
+digitalWrite(LED4, LOW);
+digitalWrite(LED5, LOW);
+digitalWrite(LED6, LOW);
+digitalWrite(LED7, LOW);
+digitalWrite(LED8, LOW);
+delay(200);
+digitalWrite(LED1, HIGH);
+digitalWrite(LED2, HIGH);
+digitalWrite(LED3, HIGH);
+digitalWrite(LED4, HIGH);
+digitalWrite(LED5, HIGH);
+digitalWrite(LED6, HIGH);
+digitalWrite(LED7, HIGH);
+digitalWrite(LED8, HIGH);
+delay(200);
+digitalWrite(LED1, LOW);
+digitalWrite(LED2, LOW);
+digitalWrite(LED3, LOW);
+digitalWrite(LED4, LOW);
+digitalWrite(LED5, LOW);
+digitalWrite(LED6, LOW);
+digitalWrite(LED7, LOW);
+digitalWrite(LED8, LOW);
+delay(200);
+digitalWrite(LED1, HIGH);
+digitalWrite(LED2, HIGH);
+digitalWrite(LED3, HIGH);
+digitalWrite(LED4, HIGH);
+digitalWrite(LED5, HIGH);
+digitalWrite(LED6, HIGH);
+digitalWrite(LED7, HIGH);
+digitalWrite(LED8, HIGH);
+delay(200);
+digitalWrite(LED1, LOW);
+digitalWrite(LED2, LOW);
+digitalWrite(LED3, LOW);
+digitalWrite(LED4, LOW);
+digitalWrite(LED5, LOW);
+digitalWrite(LED6, LOW);
+digitalWrite(LED7, LOW);
+digitalWrite(LED8, LOW);
+delay(200);
+digitalWrite(LED1, HIGH);
+digitalWrite(LED2, HIGH);
+digitalWrite(LED3, HIGH);
+digitalWrite(LED4, HIGH);
+digitalWrite(LED5, HIGH);
+digitalWrite(LED6, HIGH);
+digitalWrite(LED7, HIGH);
+digitalWrite(LED8, HIGH);
+delay(200);
+digitalWrite(LED1, LOW);
+digitalWrite(LED2, LOW);
+digitalWrite(LED3, LOW);
+digitalWrite(LED4, LOW);
+digitalWrite(LED5, LOW);
+digitalWrite(LED6, LOW);
+digitalWrite(LED7, LOW);
+digitalWrite(LED8, LOW);
+delay(200);
+digitalWrite(LED1, HIGH);
+digitalWrite(LED2, HIGH);
+digitalWrite(LED3, HIGH);
+digitalWrite(LED4, HIGH);
+digitalWrite(LED5, HIGH);
+digitalWrite(LED6, HIGH);
+digitalWrite(LED7, HIGH);
+digitalWrite(LED8, HIGH);
+delay(200);
+digitalWrite(LED1, LOW);
+digitalWrite(LED2, LOW);
+digitalWrite(LED3, LOW);
+digitalWrite(LED4, LOW);
+digitalWrite(LED5, LOW);
+digitalWrite(LED6, LOW);
+digitalWrite(LED7, LOW);
+digitalWrite(LED8, LOW);
+delay(200);
+digitalWrite(LED1, HIGH);
+digitalWrite(LED2, HIGH);
+digitalWrite(LED3, HIGH);
+digitalWrite(LED4, HIGH);
+digitalWrite(LED5, HIGH);
+digitalWrite(LED6, HIGH);
+digitalWrite(LED7, HIGH);
+digitalWrite(LED8, HIGH);
+delay(200);
+digitalWrite(LED1, LOW);
+digitalWrite(LED2, LOW);
+digitalWrite(LED3, LOW);
+digitalWrite(LED4, LOW);
+digitalWrite(LED5, LOW);
+digitalWrite(LED6, LOW);
+digitalWrite(LED7, LOW);
+digitalWrite(LED8, LOW);
+delay(200);
+
+digitalWrite(LED8, HIGH);
+delay(50);
+digitalWrite(LED7, HIGH);
+delay(50);
+digitalWrite(LED6, HIGH);
+delay(50);
+digitalWrite(LED5, HIGH);
+delay(50);
+digitalWrite(LED4, HIGH);
+delay(50);
+digitalWrite(LED3, HIGH);
+delay(50);
+digitalWrite(LED2, HIGH);
+delay(50);
+digitalWrite(LED1, HIGH);
+delay(50);
+
+digitalWrite(LED1, LOW);
+delay(50);
+digitalWrite(LED2, LOW);
+delay(50);
+digitalWrite(LED3, LOW);
+delay(50);
+digitalWrite(LED4, LOW);
+delay(50);
+digitalWrite(LED5, LOW);
+delay(50);
+digitalWrite(LED6, LOW);
+delay(50);
+digitalWrite(LED7, LOW);
+delay(50);
+digitalWrite(LED8, LOW);
+delay(50);
+
+digitalWrite(LED1, HIGH);
+digitalWrite(LED2, HIGH);
+digitalWrite(LED3, HIGH);
+digitalWrite(LED4, HIGH);
+digitalWrite(LED5, LOW);
+digitalWrite(LED6, LOW);
+digitalWrite(LED7, LOW);
+digitalWrite(LED8, LOW);
+delay(500);
+digitalWrite(LED1, LOW);
+digitalWrite(LED2, LOW);
+digitalWrite(LED3, LOW);
+digitalWrite(LED4, LOW);
+digitalWrite(LED5, HIGH);
+digitalWrite(LED6, HIGH);
+digitalWrite(LED7, HIGH);
+digitalWrite(LED8, HIGH);
+delay(500);
+digitalWrite(LED1, HIGH);
+digitalWrite(LED2, HIGH);
+digitalWrite(LED3, HIGH);
+digitalWrite(LED4, HIGH);
+digitalWrite(LED5, LOW);
+digitalWrite(LED6, LOW);
+digitalWrite(LED7, LOW);
+digitalWrite(LED8, LOW);
+delay(500);
+digitalWrite(LED1, LOW);
+digitalWrite(LED2, LOW);
+digitalWrite(LED3, LOW);
+digitalWrite(LED4, LOW);
+digitalWrite(LED5, HIGH);
+digitalWrite(LED6, HIGH);
+digitalWrite(LED7, HIGH);
+digitalWrite(LED8, HIGH);
+delay(500);
+digitalWrite(LED1, HIGH);
+digitalWrite(LED2, HIGH);
+digitalWrite(LED3, HIGH);
+digitalWrite(LED4, HIGH);
+digitalWrite(LED5, LOW);
+digitalWrite(LED6, LOW);
+digitalWrite(LED7, LOW);
+digitalWrite(LED8, LOW);
+delay(500);
+digitalWrite(LED1, LOW);
+digitalWrite(LED2, LOW);
+digitalWrite(LED3, LOW);
+digitalWrite(LED4, LOW);
+digitalWrite(LED5, LOW);
+digitalWrite(LED6, LOW);
+digitalWrite(LED7, LOW);
+digitalWrite(LED8, LOW);
+delay(500);
+}
